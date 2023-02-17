57c3ff552d7eb923b61e8fb5b5f3a7b55a9bab26
olgasawicka <sawicka@hotmail.co.uk>
2023-02-17 17:13:04 +0100

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,jsx,js}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Open Sans', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
